0e6081f17849a7c036e0cad5e949e0ef1140b97a
Connor Davidson <connordavidson93@gmail.com>
2016-09-28 11:02:01 -0600

    in class addition

--- js/game.js
+++ js/game.js
@@ -19,6 +19,17 @@ var phrases = [
     prompt("Who is your first born child?")
 ];
 
-for (var i = 0 ; i <phrases.length ; i++){
+for (var i = 0 ; i < phrases.length ; i++){
     mainPlayer.messenger(phrases[i]);
 }
+
+function playGame(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+var firstPrompt = parseInt(prompt("Enter a number between 0 and 10.") );
+var secondPrompt = parseInt(prompt("Enter a number between 11 and 20.") );
+
+var score = playGame(firstPrompt, secondPrompt);
+alert("You score is " + score + ".");
+alert("Do you want to play again?");
