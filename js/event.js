3aeccda11a66a3a4fed9e853484449dd0616156a
Connor Davidson <connordavidson93@gmail.com>
2016-09-30 11:49:22 -0600

    event handler practice

--- js/event.js
+++ js/event.js
@@ -1,14 +1,34 @@
-var button = document.getElementById("button1");
+//UserName Button
+var userName;
+console.log(userName);
 
-var myFunction = function myFunctionHandler() {
-    alert("Hello World");
+var button = document.getElementById("name");
+var enterName = function myFunctionHandler() {
+    userName = prompt("Enter your name.");
 }
-button.addEventListener ("click", myFunction);
 
+button.addEventListener ("click", enterName);
+
+//StartGame Button
 var startButton = document.getElementById("startGame");
 
 var startGameHandler = function startMyGame() {
-    prompt("Enter your name.");
+    if(userName == undefined) {
+        alert("Please enter a valid name.");
+    } else {
+        alert("Ready to play.");
+    }
 }
 
 startButton.addEventListener("click", startGameHandler);
+
+var gameButton = function GameButtonConstrcut(name) {
+    var button;
+    var fire = function fireWeaponHandler(){
+        alert("Fire");
+    }
+}
+
+var fireWeaponButton = new gameButton();
+fireWeaponButton.button = document.getElementById('fireWeapon');
+fireWeaponButton.button.addEventListener('click', fireWeaponButton.fire);
